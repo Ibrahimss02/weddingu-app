3918e92955079c1cbf2ff0c57178f6e24cdbba58
ifadatul <ifadatulkhoiroh@gmail.com>
2021-03-27 20:52:37 +0700

    ProfileActivity.kt ketinggalan

---
+++ app/src/main/java/com/drunken/weddingu/activities/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.drunken.weddingu.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.drunken.weddingu.R
+
+class ProfileActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}

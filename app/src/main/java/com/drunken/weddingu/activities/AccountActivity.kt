3277a9fdb91a9c7398d5060af926ef666e359821
ifadatul <ifadatulkhoiroh@gmail.com>
2021-04-01 14:07:20 +0700

    ini ActivityAccount wkwkw, layoutnya salah tadi

--- app/src/main/java/com/drunken/weddingu/activities/AccountActivity.kt
+++ app/src/main/java/com/drunken/weddingu/activities/AccountActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.drunken.weddingu.R
 
-class ProfileActivity : AppCompatActivity(){
+class AccountActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
